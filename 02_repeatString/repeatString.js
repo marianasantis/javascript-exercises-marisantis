cac0e47d77b544110e7512b8116cd470bcff6df1
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-02-29 16:26:02 -0300

    Solve Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let myString = '';
+  for (let i = 1; i <= num; i++) {
+    myString += string;
+  }
+  if (num >= 0) return myString;
+  else return 'ERROR';
 };
 
 // Do not edit below this line
