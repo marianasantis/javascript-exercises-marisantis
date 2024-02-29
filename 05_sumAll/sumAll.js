cc2fb5832ba4983969fa65c5e33b7d140313b328
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-02-29 16:26:39 -0300

    Solve Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+let finalSum = 1;
+const sumAll = function(num1, num2) {
 
+  if ((num1 && num2) >= 0) {
+
+    for (i = num1 + 1; i <= num2 ; i++) {
+
+      finalSum += i;
+    }
+    return finalSum;
+  }
 };
 
 // Do not edit below this line
