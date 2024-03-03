78cb5fe613c20ae7a79e633680dd75be158dacda
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-03-03 10:59:36 -0300

    Solve the exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 === 0) {
+    if (year % 100) return true;
+    else if (year % 400) return false;
+    else return true;
+  }
+  else return false;
 };
 
 // Do not edit below this line
