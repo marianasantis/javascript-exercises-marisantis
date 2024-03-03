14c516d07a0107a322a94d70a2ae7f5ac5bfcc88
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-03-03 11:00:07 -0300

    Solve the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function([...nums]) {
+  let result = 0;
+  nums.forEach(num => result += num);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function([...nums]) {
+  let result = 1;
+  nums.forEach(num => result *= num);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+	let result = num;
+  for (i = 1; i < pwr; i++) result *= num;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	for (i = num; i > 1; i--) result *= i;
+  return result;
 };
 
 // Do not edit below this line
