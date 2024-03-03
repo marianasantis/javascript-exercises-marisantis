e5d95326aecf817a75773960ecd42a76b728485a
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-03-03 11:00:34 -0300

    Solve the exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let noPunct = string.replaceAll(/[!|,|.| ]/g, '') // /g used to replace the characters inside [] globally
+  let myString = noPunct.toLowerCase();
+  const myArray = myString.split('');
+  const reversedString = myArray.reverse().join('');
 
+  return myString === reversedString;
 };
 
 // Do not edit below this line
