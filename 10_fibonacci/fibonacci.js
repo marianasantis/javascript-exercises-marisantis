41056f41eb8a3eb33f62da2d30fcbd12bc5caf49
marianasantis <134146323+marianasantis@users.noreply.github.com>
2024-03-03 11:52:48 -0300

    Solve fibonacci.js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+  if (member < 0 ) return 'OOPS';
+  const myArray = [0,1];
+  for (i = 2; i <= member; i++) {
+    let sum = myArray[myArray.length - 1] + myArray[myArray.length - 2];
+    myArray.push(sum);
+  }
+  return myArray[member];
 };
 
 // Do not edit below this line
